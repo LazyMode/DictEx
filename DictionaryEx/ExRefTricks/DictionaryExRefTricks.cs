05b319686d06e91deddbbb57ab585f53e5404e8c
Licshee <licshee@yen.ai>
2016-03-16 18:21:29 +0800

    .

---
+++ DictionaryEx/ExRefTricks/DictionaryExRefTricks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExRefTricks
+{
+    static partial class DictionaryExRefTricks
+    {
+        public static bool TryGetFirst<TKey, TValue>(this IEnumerable<IDictionary<TKey, TValue>> dicts,
+            TKey key, ref TValue value)
+        {
+            foreach (var dict in dicts)
+            {
+                if (dict.TryGetValue(key, out value))
+                    return true;
+            }
+
+            return false;
+        }
+        public static bool TryGetFirst<TKey, TValue>(TKey key, ref TValue value,
+            params IDictionary<TKey, TValue>[] dicts)
+        => dicts.TryGetFirst(key, ref value);
+
+        public static bool TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> source,
+            TKey key, ref TValue value)
+        {
+            TValue tmp;
+            if (source.TryGetValue(key, out tmp))
+            {
+                value = tmp;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
